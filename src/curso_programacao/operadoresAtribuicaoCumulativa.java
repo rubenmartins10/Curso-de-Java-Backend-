f5bfd6c556686834bba6ef9d6210194c63e7414e
rubenmartins10 <rubendavidsilvamartins@gmail.com>
2025-12-06 01:49:52 +0000

    Operadores Atribuicao Cumulativa

---
+++ src/curso_programacao/operadoresAtribuicaoCumulativa.java
@@ -0,0 +1,28 @@
+package curso_programacao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class operadoresAtribuicaoCumulativa {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+		int minutos = sc.nextInt();
+		
+		double conta = 50.0;
+		if ( minutos > 100 ) {
+			//
+			conta += (minutos - 100) * 2.0; //mesma coisa 
+		}
+		
+		System.out.printf("Valor da conta = R$ %.2f%n", conta);
+		
+		sc.close();
+	}
+
+}
+	
