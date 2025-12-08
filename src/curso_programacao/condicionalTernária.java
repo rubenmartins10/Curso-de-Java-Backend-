ddd8ec03158cedea60915b0579db60c2f80791ce
rubenmartins10 <rubendavidsilvamartins@gmail.com>
2025-12-08 23:48:22 +0000

    Condicional Ternária

---
+++ src/curso_programacao/condicionalTernária.java
@@ -0,0 +1,29 @@
+package curso_programacao;
+
+import java.util.Scanner;
+
+public class condicionalTernária {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		/* podemos fazer assim : 
+		double preco = 34.5;
+		double desconto;
+		if (preco < 20.0) {
+			desconto = preco * 0.1;
+		}
+		else {
+			desconto = preco * 0.05;
+		}
+		*/
+		double preco = 34.5;
+		double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+		System.out.println(desconto);
+		
+		sc.close();
+
+	}
+
+}
