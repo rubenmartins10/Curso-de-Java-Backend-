07bdf76b51b54ed80921f5661f0332dffa664e8d
rubenmartins10 <rubendavidsilvamartins@gmail.com>
2026-01-16 21:27:57 +0000

    Outro Exercício Exemplo (aula 74)

--- src/entitites/Product.java
+++ src/entitites/Product.java
@@ -1,5 +1,21 @@
 package entitites;
 
 public class Product {
+	
+	public String name;
+	public double price;
+	public int quantity;
+	
+	public double totalValueInStock() {
+		return price * quantity;
+	}
+	
+	public void addProducts(int quantity) {
+		this.quantity += quantity; //significa uma autorefência para o objeto, neste caso ele quer chamar o atributo da classe e não o parâmetro da função
+	}
+	
+	public void removeProducts(int quantity) {
+		this.quantity -= quantity; //igual mas para remover produtos
+	}
 
 }
--- src/problemasExemplo/aula74_outroProblemaExemplo.java
+++ src/problemasExemplo/aula74_outroProblemaExemplo.java
@@ -1,9 +1,28 @@
 package problemasExemplo;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entitites.Product;
+
 public class aula74_outroProblemaExemplo {
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Product product = new Product();
+		System.out.println("Enter product data: ");
+		System.out.println("Name: ");
+		product.name = sc.nextLine(); //faz a leitura do nome e guarda dentro da variável product no campo name
+		System.out.println("Price: ");
+		product.price = sc.nextDouble();
+		System.out.println("Quantity in stock: ");
+		product.quantity = sc.nextInt();
+		
+		
+		sc.close();
 
 	}
 
