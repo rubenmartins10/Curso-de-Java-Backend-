8c204e6622acc7f6aab7b010b4d0b6f54927d73c
rubenmartins10 <rubendavidsilvamartins@gmail.com>
2026-01-22 23:12:08 +0000

    Tipos Referência e Tipos Valor

---
+++ src/curso_programacao/tiposReferenciaTiposValor.java
@@ -0,0 +1,14 @@
+package curso_programacao;
+
+public class tiposReferenciaTiposValor {
+
+	public static void main(String[] args) {
+		
+		int p; //variável guardada na stack
+		p = 10;
+		System.out.println(p); 
+		//temos de inicializar a variável com o valor se não ela não é lida
+		// tem de ter obrigatoriamente alguma coisa
+	}
+
+}
