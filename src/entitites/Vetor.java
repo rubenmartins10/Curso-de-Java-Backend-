2b829d9377e689000ee41f4994096dd79104e344
rubenmartins10 <rubendavidsilvamartins@gmail.com>
2026-01-31 17:03:30 +0000

    Exercício 1 de Vetores

---
+++ src/entitites/Vetor.java
@@ -0,0 +1,19 @@
+package entitites;
+
+public class Vetor {
+	public Vetor(double price) {
+		this.price = price;
+	}
+
+	private double price;
+
+	public double getNumero() {
+		return price;
+	}
+
+	public void setNumero(double price) {
+		this.price = price;
+	}
+	
+	
+}
---
+++ src/exerciciosVetores/exercicio1.java
@@ -0,0 +1,33 @@
+package exerciciosVetores;
+
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class exercicio1 {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in); 
+		
+		int n = sc.nextInt();
+		int[] vect = new int[n];
+		
+		for (int i = 0; i < n; i++) {
+            System.out.print("Digite um numero: ");
+            vect[i] = sc.nextInt(); 
+        }
+		
+		for (int i = 0; i < n; i++) {
+            if (vect[i] < 0 && vect[i] < 10) {
+                System.out.println(vect[i]);
+            }
+        }
+		
+		sc.close();
+		
+		
+	}
+
+}
