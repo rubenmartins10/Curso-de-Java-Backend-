4c776d2e388bde18d0d28bbd1555533af0de9f8e
Rúben Martins <rubendavidsilvamartins@gmail.com>
2026-03-05 10:48:20 +0000

    Exercise about vectors

---
+++ src/exerciciosVetores/ExerciseAboutVectors.java
@@ -0,0 +1,47 @@
+package exerciciosVetores;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entitites.Rent;
+
+public class ExerciseAboutVectors {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+		Rent[] vect = new Rent[10];
+		
+		System.out.println("Jow many rooms will be rented? ");
+		int n = sc.nextInt();
+		
+		for ( int i = 0; i <= n; i++) {
+			System.out.println();
+			System.out.println("Rent #" + i + ":");
+			System.out.println("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.println("Email: ");
+			String email = sc.next();
+			System.out.print("Room: ");
+			int roomNumber = sc.nextInt();
+			
+			vect[roomNumber] = new Rent(name, email);
+		}
+		
+		System.out.println();
+		System.out.println("Busy rooms: ");
+		
+		for ( int i = 0; i < 10; i++) {
+			if ( vect[i] != null ) {
+				System.out.println(i + ": " + vect[i]);
+			}
+		}
+		
+		sc.close();
+	}
+
+}
